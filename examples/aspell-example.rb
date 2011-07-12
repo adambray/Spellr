87028bc059ce95cb6f34db45f2f05096f6514c88
Adam Bray <adam.bray@yale.edu>
2011-07-12 03:13:29 -0400

    early work on a method to check a whole string

--- examples/aspell-example.rb
+++ examples/aspell-example.rb
@@ -5,4 +5,11 @@ require File.expand_path('../../lib/speller', __FILE__)
 my_checker = Speller::Checker.new
 puts my_checker.correct?("word")
 puts my_checker.correct?("wordd")
-puts my_checker.suggest("wordd")
+puts my_checker.suggest("wordd").join(", ")
+result = my_checker.check_string("This is a a testz")
+if result
+  puts "The word '#{result[0]}' is misspelled at offset #{result[1]}"
+  puts "Suggested spellings: #{result[2].join(", ")}"
+else
+  puts "No mispellings found"
+end
--- lib/speller/checker.rb
+++ lib/speller/checker.rb
@@ -1,6 +1,8 @@
 module Speller
   class Checker
     
+    #Initialize ASpellSpeller, with the minimum required configuration, language. 
+    #I'd like to refactor this to either take an ASpell_Config or just a language
     def initialize(lang = "en_US")
       @config = Speller::ASpellWrapper.new_aspell_config
       Speller::ASpellWrapper.aspell_config_replace(@config,"lang", lang)
@@ -23,5 +25,31 @@ module Speller
       end
       return suggestions
     end
+  
+    def check_string(string, reset=false)
+      reset ? @offset = 0 : @offset ||= 0
+      puts @offset
+      #if we've reached the end of the string
+      return nil if @offset >= string.length + 1
+      
+      #build each word
+      word = ""
+      until string[@offset,1] == " " or @offset == (string.length) do #stop when we hit a word boundry
+        word += string[@offset,1]
+        @offset += 1
+      end
+      @offset += 1
+      
+      return check_string(string) if (word == " " || word == "")
+      
+      if !correct?(word)
+        return [word, @offset - (word.length + 1), suggest(word)]
+      else
+        puts word
+        return check_string(string)
+      end
+    end
+
   end
 end
+
